88875106738c159130dd35fde7260780d47eb4a4
kian rayaneh <hamedsojodi1374@gmail.com>
2022-06-09 17:00:16 +0430

    models complete

--- djangoProject/accounts/models.py
+++ djangoProject/accounts/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    Gender_Choices = (
+        ("m","male"),
+        ("f","female"),
+    )
+    gender = models.CharField(max_length=1, choices=Gender_Choices)
+    phone = models.CharField(max_length=15)
--- djangoProject/charities/models.py
+++ djangoProject/charities/models.py
@@ -1,13 +1,40 @@
 from django.db import models
 
+from accounts.models import User
+
 
 class Benefactor(models.Model):
-    pass
+    CHOICES = (
+        ('0', 'low'),
+        ('1', 'midlevel'),
+        ('2', 'advanced'),
+    )
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    experience = models.SmallIntegerField(choices=CHOICES, default='0')
+    free_time_per_week = models.PositiveSmallIntegerField(default='0')
 
 
 class Charity(models.Model):
-    pass
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    name = models.CharField(max_length=50)
+    reg_number = models.CharField(max_length=10)
 
 
 class Task(models.Model):
-    pass
+    CHOICES = (
+        ('P', 'Pending'),
+        ('W', 'Waiting'),
+        ('A', 'Assigned'),
+        ('D', 'Done'),
+    )
+    CHOICES2 = (
+        ('M','Male'),
+        ('F','Female'),
+
+    )
+    assigned_benefactor = models.ForeignKey(Benefactor, on_delete=models.SET_NULL, blank=True,
+                                            null=True, )
+    charity = models.ForeignKey(Charity, models.CASCADE)
+    state = models.CharField(max_length=1, choices=CHOICES, default='P')
+    gender_limit = models.CharField(max_length=1, choices=CHOICES2)
+    titel = models.CharField(max_length=100)
