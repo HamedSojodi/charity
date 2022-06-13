2594c92e6a9afbd66ac63ab438dafd3a1e6b3b2c
kian rayaneh <hamedsojodi1374@gmail.com>
2022-06-13 16:51:43 +0430

    create accounts for rest_framework

--- djangoProject1/accounts/models.py
+++ djangoProject1/accounts/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+from accounts.validators import phone_validator
+
 
 class User(AbstractUser):
-    Gender_Choices = (
-        ("m", "male"),
-        ("f", "female"),
-    )
-    gender = models.CharField(max_length=1, choices=Gender_Choices, null=True, blank=True)
-    phone = models.CharField(max_length=15)
+    class Gender(models.TextChoices):
+        MALE = 'M', 'Male'
+        FEMALE = 'F', 'Female'
+        UNSET = 'MF', 'Unset'
+    gender = models.CharField(max_length=2, choices=Gender.choices, default=Gender.UNSET, null=True, blank=True)
+    phone = models.CharField(max_length=15, validators=[phone_validator], blank=True)
     address = models.TextField(null=True, blank=True)
-    age = models.PositiveSmallIntegerField(default=True)
+    age = models.PositiveSmallIntegerField(default=True, blank=True, null=True)
     description = models.TextField(null=True, blank=True)
     last_login = models.DateTimeField(auto_now=True)
     first_name = models.CharField(max_length=100)
