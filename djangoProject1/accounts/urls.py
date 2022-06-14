d45eb42341b35e42e077f45691d614cbb84dbfec
kian rayaneh <hamedsojodi1374@gmail.com>
2022-06-14 17:22:30 +0430

    api register successfully

--- djangoProject1/accounts/urls.py
+++ djangoProject1/accounts/urls.py
@@ -3,6 +3,7 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 from .views import UserRegistration, LogoutAPIView
 
+app_name = 'accounts'
 urlpatterns = [
     path('login/', obtain_auth_token),
     path('logout/', LogoutAPIView.as_view()),
--- djangoProject1/accounts/views.py
+++ djangoProject1/accounts/views.py
@@ -4,6 +4,7 @@ from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from .models import User
 from .serializers import UserSerializer
 
 
@@ -11,5 +12,18 @@ class LogoutAPIView(APIView):
     pass
 
 
+# class UserRegistration(generics.CreateAPIView):
+#     serializer_class = UserSerializer
+#
+#     def perform_create(self, serializer):
+#         serializer.save(user=self.request.user)
+
 class UserRegistration(APIView):
-    pass
+    def post(self, request):
+        ser_data = UserSerializer(data=request.POST)
+        if ser_data.is_valid():
+            User.objects.create_user(username=ser_data.validated_data['username'],
+                                     password=ser_data.validated_data['password'],
+                                     first_name=ser_data.validated_data['first_name'])
+            return Response(data=ser_data.data)
+        return Response(data=ser_data.errors)
